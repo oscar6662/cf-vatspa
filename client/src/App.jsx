dbd4508b588be751e4f9009ba61f1a9f870217d0
Óskar Helgi <oscar.berenguer6@gmail.com>
2021-07-29 22:20:48 +0000

    routing to training

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,26 @@
 import { Route, Switch, Redirect, BrowserRouter as Router } from 'react-router-dom';
 import React, {useState, useEffect} from 'react';
+import ReactLoading from 'react-loading';
 import Index from "./views/Index/Index";
 import Profile from './views/Profile/Profile';
-import Layout from "./components/Layout/Layout"
+import Layout from "./components/Layout/Layout";
 
 import './assets/styles/config.scss';
 import './assets/styles/grid.scss';
+import TrainingMain from './views/Training/TrainingMain';
 
 
 
 export default function App() {
   const [loggedIn, setLoggedIn] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
     const fetchData = async () => {
       setIsLoading(true);
       const result = await fetch('/api/authenticated');
       const json = await result.json();
+      console.log(json);
       setLoggedIn(json.loggedIn);
       setIsLoading(false);
     };
@@ -26,23 +29,26 @@ export default function App() {
 
   return (
     isLoading ? (
-      <p>Loading Content</p>
+      <ReactLoading type={'bubble'} color={'black'} height={'20%'} width={'20%'} />
     ) : (
       
-      <Router>
         <Switch>
-          <Route exact path="/" >
-            {loggedIn ? <Redirect to="/profile" /> : <Index></Index> }
-          </Route>
-          {loggedIn && (
+        {loggedIn ? (
             <Layout>
-              <Route exact path="/profile" children={ <Profile/> } />
+              <Route path="/training" children={ <TrainingMain/> } />
+              <Route path="/profile" children={ <Profile/> } />
+              <Route exact path="/" >
+                <Redirect to="/profile" />
+             </Route>
             </Layout>
-          )
-          }
+          ) : (
+            <Route exact path="/" >
+                <Index/>
+             </Route>
+          )}
+          
           <Redirect from="*" to="/"/>
         </Switch> 
-      </Router> 
     
     )
   );
--- client/src/views/Training/TrainingMain.jsx
+++ client/src/views/Training/TrainingMain.jsx
@@ -1,9 +1,9 @@
 import React, {useEffect, useState} from 'react';
+import ReactLoading from 'react-loading';
 
-import Load from '../../components/Load/Load';
-import s from './Profile.module.scss';
+import s from './TrainingMain.module.scss';
 
-export default function Profile(){
+export default function TrainingMain(){
     const [isLoading, setIsLoading] = useState(true);
     const [isError, setIsError] = useState(false);
     const [data, setData] = useState([]);
@@ -11,6 +11,7 @@ export default function Profile(){
   
      useEffect(() => {
       const fetchData = async () => {
+          console.log("estem");
         setIsLoading(true);
         try {
             const r = await fetch('/api/user/reqtraining');
@@ -34,7 +35,7 @@ return(
        </div>
        {isLoading ? (
            <div className={s.main__loading}>
-            <Load/>
+            <ReactLoading type={'bubble'} color={'black'} height={'20%'} width={'20%'} />
            </div>
        ):(
            isError ? (
@@ -44,57 +45,7 @@ return(
            ):(
                 <div className="">
                      <div className={s.main__content}>
-                          <div className={s.main__stats}>
-                              <div className={s.main__stats__box}>
-                                  <p>Rango actual</p>
-                                  <h2>{data.vatsim.rating.short+' '+data.vatsim.rating.long}</h2>
-                              </div>
-                              <div className={s.main__stats__box}>
-                                  <p>vACC ascrito</p>
-                                  <h2>{data.vatsim.subdivision.name}</h2>
-                              </div>
-                              <div className={s.main__stats__box}>
-                                  <p>Último Training</p>
-                                  <h2>28 de Agosto</h2>
-                              </div>
-                          </div>
-                          <div className="row">
-                             <div className="col-7">
-                                <div className = {s.main__trainings}>
-                                    <h2>Mis Trainings</h2>
-                                    <table>
-                                        <tr>
-                                            <th>Training</th>
-                                            <th>Fecha</th>
-                                            <th>Resultado</th>
-                                            <th>Comentarios</th>
-                                        </tr>
-                                        <tr>
-                                            <td>Introducción S1</td>
-                                            <td>28 de Agosto</td>
-                                            <td>Aprovado</td>
-                                            <td></td>
-                                        </tr>
-                                    </table>
-                                </div>
-                             </div>
-                             <div className="col-5">
-                                 <div className = {s.main__req}>
-                                     <h2>Solicitar Training</h2>
-                                     <div  href ="#" className={s.main__req__box}>
-                                        {validTraining ? (
-                                            <a href = "/req/training">Empieza tu formación</a>
-                                        ):(
-                                            <div>
-                                                <a href ="https://vatspa.es/normativa/transferencia-VACC">Transferirte a VATSPA</a>    
-                                                <a href = "/req/training/visitor">Solicitar entrenamiento para ser Visitante</a>    
-                                            </div>
-                                        )}
-                                    </div>
-                                 </div>
-
-                             </div>
-                          </div>
+                         <p>hola</p> 
                      </div>
                 </div>
            )
