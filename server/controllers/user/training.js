e7e2d980f5b046b162a7812d2d0b3ea405bdc06f
Óskar Helgi <oscar.berenguer6@gmail.com>
2022-01-17 19:04:25 +0000

    test... mariadb

--- server/controllers/user/training.js
+++ server/controllers/user/training.js
@@ -10,8 +10,8 @@ export const router = express.Router();
 
 router.use(cookieParser());
 router.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin',"http://localhost:3000");
-  res.setHeader('Access-Control-Allow-Headers',"*");
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Headers', '*');
   res.header('Access-Control-Allow-Credentials', true);
   next();
 });
